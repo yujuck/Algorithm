20412bce39302d67996e7babd93f38fdc1fe237e
yujuck <yujin3278@gmail.com>
2021-05-10 18:10:50 +0900

    [프로그래머스] 괄호 회전하기

--- 프로그래머스/월간코드챌린지2-괄호_회전하기.js
+++ 프로그래머스/월간코드챌린지2-괄호_회전하기.js
@@ -1,17 +1,7 @@
-/*
-    문제 조건
-    1. 문자열 길이만큼 왼쪽으로 shift (shift 되서 다시 뒤로 push됨)
-    2. 문자열은 기호로만 이루오진 최대 1000개의 길이를 가짐
-    3. 괄호의 짝이 정상적으로 모두 맞춰진 경우의 갯수를 리턴
-    ex) "[]<>{}"의 경우 문자열의 길이인 6만큼 반복을 함
-    "]<>{}[", "<>{}[]", ">{}[]", "{}[]<>", "}[]<>", "[]<>{}"
-    위에서 짝이 맞는 경우는 2번, 4번, 6번 총 3개이므로 리턴은 3이 되면 된다.
- */
-
-function test(str) {
-    let arr = str.split('');
+function solution(s) {
+    let arr = s.split('');
     let count = 0;
-    
+
     for (let i in arr) {
         matchingBracket(arr) ? count++ : null;
 
