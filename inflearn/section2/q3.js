17ccb5fe20718db548a2f797f6816f79eb2c43c0
yujuck <yujin3278@gmail.com>
2021-09-30 19:22:24 +0900

    [inflearn] section2 - q3, q4

--- inflearn/section2/q3.js
+++ inflearn/section2/q3.js
@@ -1,6 +1,20 @@
 // 가위바위보
 
-function solution(a, b) {}
+function solution(a, b) {
+    const answer = a.reduce((acc, cur, idx) => {
+        let result = "";
+        if ((a[idx] === 1 && b[idx] === 3) || (a[idx] === 3 && b[idx] === 1)) {
+            result = a[idx] > b[idx] ? "B" : "A";
+        } else if (a[idx] === b[idx]) {
+            result = "D";
+        } else {
+            result = a[idx] > b[idx] ? "A" : "B";
+        }
+        return [...acc, result];
+    }, []);
+
+    return answer;
+}
 
 let a = [2, 3, 3, 1, 3];
 let b = [1, 1, 2, 2, 3];
--- inflearn/section2/q4.js
+++ inflearn/section2/q4.js
@@ -1,6 +1,19 @@
 // 점수 계산
 
-function solution(arr) {}
+function solution(arr) {
+    const withoutZero = arr.join("").split(0);
+    const correctList = withoutZero.filter(Boolean);
+
+    const answer = correctList.reduce((acc, cur) => {
+        let score = 0;
+        for (let i = 0; i < cur.length; i++) {
+            score += i + 1;
+        }
+        return acc + score;
+    }, 0);
+
+    return answer;
+}
 
 let arr = [1, 0, 1, 1, 1, 0, 0, 1, 1, 0];
 console.log(solution(arr));
