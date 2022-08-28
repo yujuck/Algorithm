a4ec4338b8342ba6223c6e973028e370968df1ce
yujuck <45589219+yujuck@users.noreply.github.com>
2022-08-28 22:15:01 +0900

    [inflearn] solve section4 q3

--- inflearn/section3/q4.js
+++ inflearn/section3/q4.js
@@ -2,19 +2,6 @@
 
 function solution(s, t){
     const answer = [];
-
-    for (let x of s.split('e')) {
-        const res = [];
-        for(let i = 0; i < x.length/2; i++) {
-            res[i] = res[x.length - 1 -i] = i + 1;
-        }
-
-        answer.push(res.join(''));
-    }
-    return answer.join('0');
-
-    // 방법 2
-    const answer = [];
     let p = s.length + 1;
 
     for(let i in s) {
--- inflearn/section4/q3.js
+++ inflearn/section4/q3.js
@@ -1,9 +1,27 @@
 // 멘토링
 
 function solution(test){
+    let answer = 0;
+    let m = test.length;
+    let n = test[0].length;
+
     for (let i = 0; i < test.length; i++) {
-        
+        for (let j = 0; j < test.length; j++) {
+            let cnt = 0;
+
+            for (let k = 0; k < m; k++) {
+                let pi = pj = 0;
+                for (let s = 0; s < n; s++) {
+                    if (test[k][s] === i) pi = i;
+                    if (test[k][s] === j) pj = j;
+                }
+
+                if (pi < pj) cnt++;
+            }
+            if (cnt === m) answer++;
+        }
     }
+    return answer;
 }
 
 let arr=[[3, 4, 1, 2], [4, 3, 2, 1], [3, 1, 4, 2]];
